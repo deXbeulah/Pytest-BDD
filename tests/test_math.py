6e83dcf21ad056faf13f5a475f3222c466b26752
Adedapo Adedamola <adedadamola@gmail.com>
2022-01-06 00:02:57 +0100

    python math operation test script.

---
+++ tests/test_math.py
@@ -0,0 +1,18 @@
+"""
+This module contains basic unit tests for math operations.
+Their purpose is to show how to use the pytest framework by example.
+"""
+
+# -------------------------------------------------------------------
+# imports
+# -------------------------------------------------------------------
+
+import pytest 
+
+
+# -------------------------------------------------------------------
+# A most basic test function
+# -------------------------------------------------------------------
+
+def test_one_plus_one():
+    assert 1 + 1 == 2
