fa47a84a08f3b86f9848136f306c1da567fbb1a9
Adedapo Adedamola <adedadamola@gmail.com>
2022-01-24 21:31:35 +0100

    Test case with a exception.

--- tests/test_math.py
+++ tests/test_math.py
@@ -17,5 +17,8 @@ import pytest
 def test_one_plus_one():
     assert 1 + 1 == 2
 
-def test_one_minus_one():
-    assert 1 - 1 == 2
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert 'division by zero' in str(e.value)
